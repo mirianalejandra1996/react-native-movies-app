feb3fba60ee8aeb6edd4243b14f97786c2b59ce8
mirian <mirianalejandra1996@gmail.com>
2025-11-06 23:31:54 -0500

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,14 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import React from 'react'
+import { Text, View } from 'react-native'
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import '../global.css'
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
+const RootLayout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    <View>
+      <Text className='text-3xl'>Holaaa</Text>
+    </View>
+  )
 }
+
+export default RootLayout
